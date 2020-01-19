ae555cdaeec7cb0ac4c27958f9688b80b114f938
deanzahavy <deanzahavy@ST-219-PC>
2020-01-19 10:26:23 +0200

    final version

--- group71_ex4_client/client_main.c
+++ group71_ex4_client/client_main.c
@@ -3,26 +3,18 @@
    Project     - client_main.
    Description - this file runs the program.
 */
-#define _CRT_MAP_ALLOC
+
 #include "Client.h"
 #include "string.h"
 #include <stdlib.h>
 #include "DefenitionsForServerClient.h"
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#else
-#define _ASSERT(expr) ((void)0)
 
-#define _ASSERTE(expr) ((void)0)
-#endif
 
 int main(int argc, char* argv[])
 {	
 	// Check if got all arguments.
 	if (argc == 4)
-	{
-		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
+	{	
 		// run client.
 		if (strlen(argv[3]) > MAX_NAME_SIZE)
 		{
